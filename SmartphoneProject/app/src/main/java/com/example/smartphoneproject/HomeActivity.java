839db46f12c53f3b990929639f8998d42b09f6ed
HefnaValravn <nicolabenedetti2005@gmail.com>
2025-11-05 11:51:19 +0200

    started app project. Added about half of the required elements

---
+++ SmartphoneProject/app/src/main/java/com/example/smartphoneproject/HomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.smartphoneproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.example.smartphoneproject.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent in = getIntent();
+
+        String nameValue = in.getStringExtra(FirstActivity.NAME);
+        TextView tv = findViewById(R.id.tvWelcome);
+        tv.setText("Hello " + nameValue + "!\nWe are very happy to see you.");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+}
